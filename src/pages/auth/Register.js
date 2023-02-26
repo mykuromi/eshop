dad118ef85534ce1359788370605073b1e4cbd2c
Heera <92657209+mykuromi@users.noreply.github.com>
2023-02-26 15:10:08 +0900

    3.4 Reset page JSX

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,8 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Card from "../../components/card/Card";
 import styles from "./auth.module.scss";
+import registerImg from "../../assets/register.png";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <section className={`container ${styles.auth}`}>
+      <Card>
+        <div className={styles.form}>
+          <h2>Register</h2>
+          <form>
+            <input type="text" placeholder="Email" required />
+            <input type="password" placeholder="Password" required />
+            <input type="password" placeholder="Confirm Password" required />
+            <button className="--btn --btn-primary --btn-block">
+              Register
+            </button>
+          </form>
+          <span className={styles.register}>
+            <p>Aleady have an account?</p>
+            <Link to="/login">Login</Link>
+          </span>
+        </div>
+      </Card>
+      <div className={styles.img}>
+        <img src={registerImg} alt="Register" width="400" />
+      </div>
+    </section>
+  );
 };
 
 export default Register;
--- src/pages/auth/Reset.js
+++ src/pages/auth/Reset.js
@@ -1,8 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Card from "../../components/card/Card";
 import styles from "./auth.module.scss";
+import resetImg from "../../assets/forgot.png";
 
 const Reset = () => {
-  return <div>Reset</div>;
+  return (
+    <section className={`container ${styles.auth}`}>
+      <div className={styles.img}>
+        <img src={resetImg} alt="Reset Password" width="400" />
+      </div>
+      <Card>
+        <div className={styles.form}>
+          <h2>Reset Password</h2>
+          <form>
+            <input type="text" placeholder="Email" required />
+            <button className="--btn --btn-primary --btn-block">
+              Reset Password
+            </button>
+            <div className={styles.links}>
+              <p>
+                <Link to="/login">- Login</Link>
+              </p>
+              <p>
+                <Link to="/register">- Register</Link>
+              </p>
+            </div>
+          </form>
+        </div>
+      </Card>
+    </section>
+  );
 };
 
 export default Reset;
