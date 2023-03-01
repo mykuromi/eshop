bd6499e6317afcf26c56d59374b74e6d5ab6c27c
Heera <92657209+mykuromi@users.noreply.github.com>
2023-03-01 09:05:25 +0900

    4.2 Sign in with email and password

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -27,7 +27,6 @@ const Register = () => {
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         const user = userCredential.user;
-        console.log(user);
         setIsLoading(false);
         toast.success("Registration successful...");
         navigate("/login");
