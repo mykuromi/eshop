8fdf6a07986bd84dd58e5b0c3f4dc4d2bc064552
Heera <92657209+mykuromi@users.noreply.github.com>
2023-03-23 05:38:18 +0900

    7.4 Create fallback content for non-admin

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -9,7 +9,9 @@ import { toast } from "react-toastify";
 import { useDispatch } from "react-redux";
 import { SET_ACTIVE_USER } from "../../redux/slice/authSlice";
 import ShowOnLogin, { ShowOnLogout } from "../hiddenLink/hiddenLink";
-import AdminOnlyRoute from "../adminOnlyRoute/AdminOnlyRoute";
+import AdminOnlyRoute, {
+  AdminOnlyLink,
+} from "../adminOnlyRoute/AdminOnlyRoute";
 
 const logo = (
   <div className={styles.logo}>
@@ -107,9 +109,11 @@ const Header = () => {
               <FaTimes size={22} color="#fff" onClick={hideMenu} />
             </li>
             <li>
-              <AdminOnlyRoute>
-                <button className="--btn --btn-primary">Admin</button>
-              </AdminOnlyRoute>
+              <AdminOnlyLink>
+                <Link to="/admin/home">
+                  <button className="--btn --btn-primary">Admin</button>
+                </Link>
+              </AdminOnlyLink>
             </li>
             <li>
               <NavLink to="/" className={activeLink}>
