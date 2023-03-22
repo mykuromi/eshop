b9766474eebf089b916f9f3a05ac83cc3efaa289
Heera <92657209+mykuromi@users.noreply.github.com>
2023-03-23 06:18:56 +0900

    8.1 Add product component JSX

--- src/components/admin/addProduct/AddProduct.js
+++ src/components/admin/addProduct/AddProduct.js
@@ -1,7 +1,119 @@
+import { useState } from "react";
+import Card from "../../card/Card";
 import styles from "./AddProduct.module.scss";
 
 const AddProduct = () => {
-  return <div>AddProduct</div>;
+  const categories = [
+    { id: 1, name: "Laptop" },
+    { id: 2, name: "Electronics" },
+    { id: 3, name: "Fashion" },
+    { id: 4, name: "Phone" },
+  ];
+
+  const [product, setProduct] = useState({
+    name: "",
+    imageURL: "",
+    price: "",
+    category: "",
+    brand: "",
+    desc: "",
+  });
+
+  const handleInputChange = (e) => {};
+  const handleImageChange = (e) => {};
+
+  return (
+    <div className={styles.product}>
+      <h1>Add New Product</h1>
+      <form>
+        <Card cardClass={styles.card}>
+          <label>Product Name:</label>
+          <input
+            type="text"
+            placeholder="Product Name"
+            required
+            name="name"
+            value={product.name}
+            onChange={(e) => handleInputChange(e)}
+          />
+
+          <label>Product Image:</label>
+          <Card cardClass={styles.group}>
+            <div className={styles.progress}>
+              <div className={styles["progress-bar"]} styles={{ width: "50%" }}>
+                Uploading 50%
+              </div>
+            </div>
+            <input
+              type="file"
+              accept="image/*"
+              placeholder="Product Image"
+              name="image"
+              onChange={(e) => handleImageChange(e)}
+            />
+            <input
+              type="text"
+              required
+              name="imageURL"
+              value={product.imageURL}
+              disabled
+            />
+          </Card>
+
+          <label>Product Price:</label>
+          <input
+            type="text"
+            placeholder="Product Price"
+            required
+            name="price"
+            value={product.price}
+            onChange={(e) => handleInputChange(e)}
+          />
+
+          <label>Product Category:</label>
+          <select
+            required
+            name="category"
+            value={product.category}
+            onChange={(e) => handleInputChange(e)}
+          >
+            <option value="" disable>
+              -- Choose Product Category --
+            </option>
+            {categories.map((cat) => {
+              return (
+                <option key={cat.id} value={cat.name}>
+                  {cat.name}
+                </option>
+              );
+            })}
+          </select>
+
+          <label>Product Company/Brand:</label>
+          <input
+            type="text"
+            placeholder="Product Brand"
+            required
+            name="brand"
+            value={product.brand}
+            onChange={(e) => handleInputChange(e)}
+          />
+
+          <label>Product Description:</label>
+          <textarea
+            name="desc"
+            required
+            value={product.desc}
+            onChange={(e) => handleInputChange(e)}
+            cols="30"
+            rows="10"
+          ></textarea>
+
+          <button className="--btn --btn-primary">Save Product</button>
+        </Card>
+      </form>
+    </div>
+  );
 };
 
 export default AddProduct;
