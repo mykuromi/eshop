059b4853f0437b123585f922aead8ecfae4870a8
Heera <92657209+mykuromi@users.noreply.github.com>
2023-04-03 00:07:40 +0900

    9.2 Display products on the page

--- src/components/admin/viewProducts/ViewProducts.js
+++ src/components/admin/viewProducts/ViewProducts.js
@@ -1,15 +1,10 @@
-import {
-  collection,
-  query,
-  where,
-  onSnapshot,
-  orderBy,
-  limit,
-} from "firebase/firestore";
+import { collection, query, onSnapshot, orderBy } from "firebase/firestore";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
 import { db } from "../../../firebase/config";
 import styles from "./ViewProducts.module.scss";
+import { FaEdit, FaTrash } from "react-icons/fa";
 
 const ViewProducts = () => {
   const [products, setProducts] = useState([]);
@@ -31,8 +26,8 @@ const ViewProducts = () => {
           id: doc.id,
           ...doc.data(),
         }));
-        console.log(allProducts);
         setProducts(allProducts);
+        setIsLoading(false);
       });
     } catch (error) {
       setIsLoading(false);
@@ -40,6 +35,57 @@ const ViewProducts = () => {
     }
   };
 
-  return <div>ViewProducts</div>;
+  return (
+    <>
+      <div className={styles.table}>
+        <h2>All Products</h2>
+        {products.length === 0 ? (
+          <p>No product fonund.</p>
+        ) : (
+          <table>
+            <thead>
+              <tr>
+                <th>s/n</th>
+                <th>Image</th>
+                <th>Name</th>
+                <th>Category</th>
+                <th>Price</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product, index) => {
+                const { id, name, price, imageURL, category } = product;
+                return (
+                  <tr key={id}>
+                    <td>{index + 1}</td>
+                    <td>
+                      <img
+                        src={imageURL}
+                        alt={name}
+                        style={{ width: "100px" }}
+                      />
+                    </td>
+                    <td>{name}</td>
+                    <td>{category}</td>
+                    <td>{`$${price}`}</td>
+                    <td>
+                      <Link to="/admin/add-product">
+                        <FaEdit size={20} color="green" />
+                      </Link>
+                      &nbsp;
+                      <Link>
+                        <FaTrash size={20} color="red" />
+                      </Link>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        )}
+      </div>
+    </>
+  );
 };
 export default ViewProducts;
