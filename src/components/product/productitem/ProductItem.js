3494818804a9bc2b239e00198abfae00c603e5bf
Heera <92657209+mykuromi@users.noreply.github.com>
2023-04-09 10:16:26 +0900

    12.4 Product item component-2

--- src/components/product/productitem/ProductItem.js
+++ src/components/product/productitem/ProductItem.js
@@ -23,6 +23,8 @@ const ProductItem = ({ product, grid, id, name, price, desc, imageURL }) => {
           <p>{`$${price}`}</p>
           <h4>{shortenText(name, 18)}</h4>
         </div>
+        {!grid && <p className={styles.desc}>{shortenText(desc, 200)}</p>}
+        <button className="--btn --btn-danger">Add to cart</button>
       </div>
     </Card>
   );
