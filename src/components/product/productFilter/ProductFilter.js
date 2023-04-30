c4ba401224a4512cdec50eefc9dc93ff8671d34d
Heera <92657209+mykuromi@users.noreply.github.com>
2023-04-30 13:15:16 +0900

    13.4 Filter products by category

--- src/components/product/productFilter/ProductFilter.js
+++ src/components/product/productFilter/ProductFilter.js
@@ -1,11 +1,41 @@
+import { useDispatch, useSelector } from "react-redux";
 import styles from "./ProductFilter.module.scss";
+import { selectProducts } from "../../../redux/slice/productSlice";
+import { useState } from "react";
+import { FILTER_BY_CATEGORY } from "../../../redux/slice/filterSlice";
 
 const ProductFilter = () => {
+  const [category, setCategory] = useState("All");
+  const products = useSelector(selectProducts);
+
+  const dispatch = useDispatch();
+
+  const allCategories = [
+    "All",
+    ...new Set(products.map((product) => product.category)),
+  ];
+
+  const filterProducts = (cat) => {
+    setCategory(cat);
+    dispatch(FILTER_BY_CATEGORY({ products, category: cat }));
+  };
+
   return (
     <div className={styles.filter}>
       <h4>Categories</h4>
       <div className={styles.category}>
-        <button>All</button>
+        {allCategories.map((cat, index) => {
+          return (
+            <button
+              key={index}
+              type="button"
+              className={`${category}` === cat ? `${styles.active}` : null}
+              onClick={() => filterProducts(cat)}
+            >
+              &#8250;{cat}
+            </button>
+          );
+        })}
       </div>
       <h4>Brand</h4>
       <div className={styles.brand}>
--- src/redux/slice/filterSlice.js
+++ src/redux/slice/filterSlice.js
@@ -17,10 +17,59 @@ const filterSlice = createSlice({
       );
       state.filteredProducts = tempProducts;
     },
+    SORT_PRODUCTS(state, action) {
+      const { products, sort } = action.payload;
+      let tempProducts = [];
+      switch (sort) {
+        case "lowest-price": {
+          tempProducts = products.slice().sort((a, b) => {
+            return a.price - b.price;
+          });
+          break;
+        }
+        case "highest-price": {
+          tempProducts = products.slice().sort((a, b) => {
+            return b.price - a.price;
+          });
+          break;
+        }
+        case "a-z": {
+          tempProducts = products.slice().sort((a, b) => {
+            return a.name.localeCompare(b.name);
+          });
+          break;
+        }
+        case "z-a": {
+          tempProducts = products.slice().sort((a, b) => {
+            return b.name.localeCompare(a.name);
+          });
+          break;
+        }
+        default: {
+          // latest
+          tempProducts = products;
+          break;
+        }
+      }
+      state.filteredProducts = tempProducts;
+    },
+    FILTER_BY_CATEGORY(state, action) {
+      const { products, category } = action.payload;
+      let tempProducts = [];
+      if (category === "All") {
+        tempProducts = products;
+      } else {
+        tempProducts = products.filter(
+          (product) => product.category === category
+        );
+      }
+      state.filteredProducts = tempProducts;
+    },
   },
 });
 
-export const { FILTER_BY_SEARCH } = filterSlice.actions;
+export const { FILTER_BY_SEARCH, SORT_PRODUCTS, FILTER_BY_CATEGORY } =
+  filterSlice.actions;
 
 export const selectFilteredProducts = (state) => state.filter.filteredProducts;
 
