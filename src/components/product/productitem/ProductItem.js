bda600b8cb18ff19f2bd57f8d19695b6a0cf902a
Heera <92657209+mykuromi@users.noreply.github.com>
2023-05-08 01:50:44 +0900

    15.2 Add products to cart

--- src/components/product/productitem/ProductItem.js
+++ src/components/product/productitem/ProductItem.js
@@ -1,14 +1,22 @@
 import { Link } from "react-router-dom";
 import Card from "../../card/Card";
 import styles from "./ProductItem.module.scss";
+import { useDispatch } from "react-redux";
+import { ADD_TO_CART } from "../../../redux/slice/cartSlice";
 
 const ProductItem = ({ product, grid, id, name, price, desc, imageURL }) => {
+  const dispatch = useDispatch();
+
   const shortenText = (text, n) => {
     if (text.length > n) {
       const shortenedText = text.substring(0, n).concat("...");
       return shortenedText;
     }
     return text;
+  };
+
+  const addToCart = (product) => {
+    dispatch(ADD_TO_CART(product));
   };
 
   return (
@@ -24,7 +32,12 @@ const ProductItem = ({ product, grid, id, name, price, desc, imageURL }) => {
           <h4>{shortenText(name, 18)}</h4>
         </div>
         {!grid && <p className={styles.desc}>{shortenText(desc, 200)}</p>}
-        <button className="--btn --btn-danger">Add to cart</button>
+        <button
+          className="--btn --btn-danger"
+          onClick={() => addToCart(product)}
+        >
+          Add to cart
+        </button>
       </div>
     </Card>
   );
--- src/redux/slice/cartSlice.js
+++ src/redux/slice/cartSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
 
 const initialState = {
   cartItems: localStorage.getItem("cartItems")
@@ -14,6 +15,23 @@ const cartSlice = createSlice({
   reducers: {
     ADD_TO_CART(state, action) {
       console.log(action.payload);
+      const productIndex = state.cartItems.findIndex(
+        (item) => item.id === action.payload.id
+      );
+
+      if (productIndex >= 0) {
+        state.cartItems[productIndex].cartQuantity += 1;
+        toast.info(`${action.payload.name} increased by one`, {
+          positon: "top-left",
+        });
+      } else {
+        const tempProduct = { ...action.payload, cartQuantity: 1 };
+        state.cartItems.push(tempProduct);
+        toast.success(`${action.payload.name} product added to cart`, {
+          positon: "top-left",
+        });
+      }
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
     },
   },
 });
