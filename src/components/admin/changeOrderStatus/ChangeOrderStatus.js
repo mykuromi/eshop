2966a6400929e533765193c371159d5f2a8cce27
Heera <92657209+mykuromi@users.noreply.github.com>
2023-05-18 22:33:40 +0900

    20.5 Change order status component jsx

--- src/components/admin/changeOrderStatus/ChangeOrderStatus.js
+++ src/components/admin/changeOrderStatus/ChangeOrderStatus.js
@@ -1,7 +1,42 @@
+import { useState } from "react";
 import styles from "./ChangeOrderStatus.module.scss";
+import Loader from "../../loader/Loader";
+import Card from "../../card/Card";
 
 const ChangeOrderStatus = () => {
-  return <div>ChangeOrderStatus</div>;
+  const [status, setStatus] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const editOrder = () => {};
+
+  return (
+    <>
+      {isLoading && <Loader />}
+      <div className={styles.status}>
+        <Card cardClass={styles.card}>
+          <h4>Update status</h4>
+          <form onSubmit={editOrder}>
+            <span>
+              <select name={status} onChange={(e) => setStatus(e.target.value)}>
+                <option value="" disabled>
+                  -- Choose one--
+                </option>
+                <option value="Order placed...">Order placed...</option>
+                <option value="Processing...">Processing...</option>
+                <option value="Shipped...">Shipped...</option>
+                <option value="Delivered">Delivered</option>
+              </select>
+            </span>
+            <span>
+              <button type="submit" className="--btn --btn-primary">
+                Update status
+              </button>
+            </span>
+          </form>
+        </Card>
+      </div>
+    </>
+  );
 };
 
 export default ChangeOrderStatus;
--- src/components/admin/orderDetails/OrderDetails.js
+++ src/components/admin/orderDetails/OrderDetails.js
@@ -1,10 +1,91 @@
+import { Link, useParams } from "react-router-dom";
 import styles from "./OrderDetails.module.scss";
+import spinnerImg from "../../../assets/spinner.jpg";
+import { useEffect, useState } from "react";
+import useFetchDocument from "../../../customHooks/useFetchDocument";
+import ChangeOrderStatus from "../changeOrderStatus/ChangeOrderStatus";
 
 const OrderDetails = () => {
+  const [order, setOrder] = useState(null);
+  const { id } = useParams();
+  const { document } = useFetchDocument("orders", id);
+
+  useEffect(() => {
+    setOrder(document);
+    console.log(document);
+  }, [document]);
+
   return (
-    <div>
-      <h2>Order details</h2>
-    </div>
+    <>
+      <div className={`container ${styles.table}`}>
+        <h2>Order details</h2>
+        <div>
+          <Link to="/order-history">&larr; Back to orders</Link>
+        </div>
+        <br />
+        {order === null ? (
+          <img src={spinnerImg} alt="Loading..." style={{ width: "50px" }} />
+        ) : (
+          <>
+            <p>
+              <b>Order ID</b> {order.id}
+            </p>
+            <p>
+              <b>Order amount</b> {order.orderAmount}
+            </p>
+            <p>
+              <b>Order status</b> {order.orderStatus}
+            </p>
+            <br />
+            <table>
+              <thead>
+                <tr>
+                  <th>S/N</th>
+                  <th>Product</th>
+                  <th>Price</th>
+                  <th>Quantity</th>
+                  <th>Total</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {order.cartItems.map((cart, index) => {
+                  const { id, name, price, imageURL, cartQuantity } = cart;
+                  return (
+                    <tr key={id}>
+                      <td>
+                        <b>{index + 1}</b>
+                      </td>
+                      <td>
+                        <p>
+                          <b>{name}</b>
+                        </p>
+                        <img
+                          src={imageURL}
+                          alt={name}
+                          style={{ width: "100px" }}
+                        />
+                      </td>
+                      <td>${price}</td>
+                      <td>{cartQuantity}</td>
+                      <td>${(price * cartQuantity).toFixed(2)}</td>
+                      <td className={styles.icons}>
+                        <Link to={`/review-product/${id}`}>
+                          <button className="--btn --btn-primary">
+                            Review product
+                          </button>
+                        </Link>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </>
+        )}
+        <ChangeOrderStatus />
+      </div>
+    </>
   );
 };
 
