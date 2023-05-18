bcf0454ec1006d300039bd5e4525663bad6afbfc
Heera <92657209+mykuromi@users.noreply.github.com>
2023-05-19 01:45:57 +0900

    21.1 Contact us JSX

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,10 +1,73 @@
+import Card from "../../components/card/Card";
 import styles from "./Contact.module.scss";
+import { FaPhoneAlt, FaEnvelope, FaTwitter } from "react-icons/fa";
+import { GoLocation } from "react-icons/go";
 
 const Contact = () => {
+  const sendEmail = () => {};
+
   return (
-    <div>
-      <h1>Contact</h1>
-    </div>
+    <section>
+      <div className={`container ${styles.contact}`}>
+        <h2>Contact</h2>
+        <div className={styles.section}>
+          <form onSubmit={sendEmail}>
+            <Card cardClass={styles.card}>
+              <label>Name</label>
+              <input
+                type="text"
+                name="user_name"
+                placeholder="Full name"
+                required
+              />
+              <label>Email</label>
+              <input
+                type="email"
+                name="user_email"
+                placeholder="Your active email"
+                required
+              />
+              <label>Subject</label>
+              <input
+                type="text"
+                name="subject"
+                placeholder="Subject"
+                required
+              />
+              <label>Message</label>
+              <textarea name="message" cols="30" rows="10"></textarea>
+              <button className="--btn --btn-primary">Send message</button>
+            </Card>
+          </form>
+          <div className={styles.details}>
+            <Card cardClass={styles.card2}>
+              <h3>Our contact information</h3>
+              <p>
+                Fill the form or contact us via other channels listed below.s
+              </p>
+              <div className={styles.icons}>
+                <span>
+                  <FaPhoneAlt />
+                  <p>+234 705 141 6545</p>
+                </span>
+                <span>
+                  <FaEnvelope />
+                  <p>support@eshop.com</p>
+                </span>
+                <span>
+                  <GoLocation />
+                  <p>Seoul, Republic of Korea</p>
+                </span>
+                <span>
+                  <FaTwitter />
+                  <p>@mykuromi.log</p>
+                </span>
+              </div>
+            </Card>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
