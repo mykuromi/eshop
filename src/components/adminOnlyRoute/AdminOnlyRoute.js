73800b09ccefb77ceed5000c80446b3a11ad76c3
Heera <92657209+mykuromi@users.noreply.github.com>
2023-05-19 13:29:10 +0900

    22.3 Setup admin accounts

--- src/components/adminOnlyRoute/AdminOnlyRoute.js
+++ src/components/adminOnlyRoute/AdminOnlyRoute.js
@@ -5,7 +5,7 @@ import { selectEmail } from "../../redux/slice/authSlice";
 const AdminOnlyRoute = ({ children }) => {
   const userEmail = useSelector(selectEmail);
 
-  if (userEmail === "test@gmail.com") {
+  if (userEmail === `${process.env.REACT_APP_ADMIN_ACCOUNT}`) {
     return children;
   } else {
     return (
@@ -26,7 +26,7 @@ const AdminOnlyRoute = ({ children }) => {
 export const AdminOnlyLink = ({ children }) => {
   const userEmail = useSelector(selectEmail);
 
-  if (userEmail === "test@gmail.com") {
+  if (userEmail === `${process.env.REACT_APP_ADMIN_ACCOUNT}`) {
     return children;
   } else {
     return null;
